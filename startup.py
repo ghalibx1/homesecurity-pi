7d9e48c24b818316a4dc82edb9e7a6880cdb1c48
Muhammad Awaiz Khan <112326336+ghalibx1@users.noreply.github.com>
2024-07-29 12:06:33 +0100

    Add files via upload

---
+++ startup.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+from os import system
+
+# Main loop
+def main():
+    print("Starting intrusion detection script in background...")
+    os.system("nohup python -u /home/pi/detection/detect.py > /dev/null 2>&1 &")
+
+
+if __name__ == "__main__":
+    main()
